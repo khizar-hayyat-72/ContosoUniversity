6f83d5cd36ed4dc045ba3e9632f1d97c307b1dc8
Khizar Hayyat Jafar <Khizarhayyt199093@gmail.com>
2022-01-21 13:19:31 +0500

    Add project files.

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Required LastName")]
+        [Display(Name = "First Name", Prompt = "Enter First Name", Description = "Student First Name")]
+        public string? LastName { get; set; }
+        [Required(ErrorMessage = "Required FirstMidName")]
+
+        public string? FirstMidName { get; set; }
+
+        [Required(ErrorMessage = "Required EneollmentDate")]
+        public DateTime EnrollmentDate { get; set; }
+
+
+        public ICollection<Enrollment>? Enrollments { get; set; }
+    }
+}
